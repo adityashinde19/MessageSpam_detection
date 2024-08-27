8a3249f6eef41b27914496aefd939e08195eb30e
adityashinde19 <aditya.shinde@aress.com>
2024-08-27 12:15:43 +0530

    app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import numpy as np
 import pickle
 import streamlit as st
 
-# Load the model, vectorizer, and label encoder
+
 with open("SMS_ham.pkl", "rb") as model_file:
     classifier = pickle.load(model_file)
 
-# with open("tfidf_vectorizer.pkl", "rb") as vectorizer_file:
-#     vectorizer = pickle.load(vectorizer_file)
+
 
 with open("SMS_label.pkl", "rb") as encoder_file:
     label_encoder = pickle.load(encoder_file)
@@ -18,18 +17,17 @@ def map_prediction(prediction):
 
 # Prediction function
 def sms_prediction(sms_text):
-    # Transform the input text using the vectorizer
-    # sms_vector = vectorizer.transform([sms_text])
+  
 
-    # Predict using the classifier
+   
     prediction = classifier.predict([sms_text])
 
-    # Map prediction to 'SPAM' or 'HAM'
+    
     return map_prediction(prediction)
 
-# Main function for Streamlit app
+
 def main():
-    # Custom CSS for styling
+    
     st.markdown("""
         <style>
             .main {background-color: #f7f7f7; padding: 10px;}
@@ -41,7 +39,7 @@ def main():
         </style>
     """, unsafe_allow_html=True)
 
-    # Title and description
+  
     st.markdown("""
     <div class="title-div">
         <h2 style="text-align:center;">SMS SPAM DETECTION SYSTEM </h2>
@@ -50,10 +48,10 @@ def main():
 
     st.markdown("<p style='text-align:center; color:#888;'>Enter an SMS message below to check if it's Spam or Ham.</p>", unsafe_allow_html=True)
 
-    # Input box for SMS description
+    
     sms_input = st.text_input("Enter the SMS Message:", placeholder="Type your message here...")
 
-    # Button for prediction
+  
     if st.button("Check Message"):
         if sms_input.strip():  # Ensure input is not empty
             with st.spinner('Analyzing the message...'):
@@ -62,13 +60,13 @@ def main():
         else:
             st.error("Please enter a valid SMS message.")
 
-    # Footer with some custom message or branding
+   
     st.markdown("""
     <div class="footer">
         <p>Powered by <strong>Aditya Shinde </strong> | SMS Spam Detection</p>
     </div>
     """, unsafe_allow_html=True)
 
-# Run the app
+
 if __name__ == '__main__':
     main()
